09e06612445b813d2232f3383c0e877cfddb6a15
Ilia Mikhailov <ilia@mikhailov.se>
2020-01-09 08:49:11 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Link, Router, Route } from 'yrv';
+</script>
+
+<div class="font-inter flex flex-grow h-screen justify-center items-center">
+  <p>
+    <Router>
+      <Route exact>Hello World</Route>
+      <Route fallback>Not found</Route>
+      <Route exact path="/:name" let:router>Hey {router.params.name}!</Route>
+    </Router>
+  </p>
+</div>
